8afb048d575caf82d7e85a0e754d4c732efaa546
Max Nabokow <max.nabokow@gmail.com>
2020-01-01 02:52:30 +0100

    Deleted Cocoapods

--- GitHubFollowers/Controller/SearchVC.swift
+++ GitHubFollowers/Controller/SearchVC.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import LBTATools
 
 class SearchVC: UIViewController {
     
@@ -50,7 +49,6 @@ class SearchVC: UIViewController {
     fileprivate func setupLayout() {
         view.addSubview(logoImageView)
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -59,12 +57,22 @@ class SearchVC: UIViewController {
         ])
         
         view.addSubview(usernameTextField)
-        usernameTextField.anchor(top: logoImageView.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 48, left: 50, bottom: 0, right: 50))
-        usernameTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
         
         view.addSubview(getFollowersButton)
-        getFollowersButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 50, right: 50))
-        getFollowersButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        getFollowersButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            getFollowersButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            getFollowersButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            getFollowersButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            getFollowersButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     // MARK: - Selectors
--- Pods/Target Support Files/LBTATools/LBTATools-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LBTATools : NSObject
-@end
-@implementation PodsDummy_LBTATools
-@end
